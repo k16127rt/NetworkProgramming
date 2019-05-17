bcfc6d88a12f3f9246bf677f6c4119f3806ae43d
k16127 <k16127rt@apps.kct.ac.jp>
2019-05-17 10:31:30 +0900

    20190517

--- client.c
+++ client.c
@@ -1,25 +1,40 @@
+#define BUF_SIZE 256
 #include<sys/socket.h>
 #include<arpa/inet.h>
 #include<unistd.h>
 #include<string.h>
 #include<stdio.h>
+#include<stdlib.h>
+void DieWithError(char *errorMessage){
+	perror(errorMessage); //標準エラー出力にエラーメッセージを出す。
+	exit(1); //終了ステータス。1以上を返すという意味。
+}
 void commun (int sock){
-	char buf[256];
+	char buf[BUF_SIZE];
 	int len_r;
-	char *message = "Hello server!";
-	send(sock, message, strlen(message), 0);
-	len_r = recv(sock, buf, 256, 0);
+	char *message = "";
+	if(send(sock, message, strlen(message), 0) != strlen(message))
+		DieWithError("send()sent a message of unexpected bytes");
+	if((len_r = recv(sock, buf, BUF_SIZE, 0)) <= 0)
+		DieWithError("recv() failed");
 	buf[len_r] = '\0';
 	printf("%s\n", buf);
 }
 int main(int argc, char**argv){
-  int sock = socket(PF_INET, SOCK_STREAM, 0);
-  struct sockaddr_in target;
-  target.sin_family = AF_INET;
-  target.sin_addr.s_addr = inet_addr("10.13.64.20");
-  target.sin_port = htons(10001);
-  connect(sock, (struct sockaddr *)&target, sizeof(target));
-  commun(sock);
-  close(sock);
-  return 0;
+	if(argc != 3)
+		DieWithError("arguments is not available");
+	char *server_ipaddr = argv[1];//"10.13.64.20"
+	int server_port = atoi(argv[2]);
+	int sock = socket(PF_INET, SOCK_STREAM, 0);
+	if(sock < 0)
+		DieWithError("socket()failed");
+	struct sockaddr_in target;
+	target.sin_family = AF_INET;
+	target.sin_addr.s_addr = inet_addr(server_ipaddr);
+	target.sin_port = htons(server_port);
+	if(connect(sock, (struct sockaddr *)&target, sizeof(target)) < 0)
+		DieWithError("connect()failed");
+	commun(sock);
+	close(sock);
+	return 0;
 }
